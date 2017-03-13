7132f29e46e75fa9f7593ec5c4c0d993d37a69bd
Daniel Franklin <d-franklin@users.noreply.github.com>
2017-03-13 11:28:39 +0700

    Updated to target netcore and netstandard.

--- Franklin.Maybe.Tests/Properties/AssemblyInfo.cs
+++ Franklin.Maybe.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,9 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Daniel Franklin">
+//   Copyright © 2017 Daniel Franklin. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Franklin.Maybe.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Daniel Franklin 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,14 +27,12 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a4030d5a-f6f5-4eab-a789-b3a35aea74e0")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
